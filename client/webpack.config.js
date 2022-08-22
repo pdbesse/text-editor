41286f33dcf4b6c7f8f007a03a0369344490ae48
Phillip <phillip.besse@gmail.com>
2022-08-21 22:26:29 -0400

    app finished

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -21,10 +21,8 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Webpack Plugin',
+        title: 'JATE',
       }),
-      // new MiniCssExtractPlugin(),
-      new WorkboxPlugin.GenerateSW(),
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
@@ -32,47 +30,39 @@ module.exports = () => {
       new WebpackPwaManifest({
         name: 'just another text editor',
         short_name: 'jate',
+        fingerprints: false,
+        inject: true,
         description: 'My awesome text editor!',
         background_color: '#ffffff',
-        crossorigin: 'anonymous', //can be null, use-credentials or anonymous
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+            destination: path.join('assets', 'icons'),
           },
-          // {
-          //   src: path.resolve('src/assets/large-icon.png'),
-          //   size: '1024x1024' // you can also use the specifications pattern
-          // },
-          // {
-          //   src: path.resolve('src/assets/maskable-icon.png'),
-          //   size: '1024x1024',
-          //   purpose: 'maskable'
-          // }
         ]
       })
-     
+
     ],
 
     module: {
       rules: [
         {
           test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
         },
         {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
         },
       ],
     },
